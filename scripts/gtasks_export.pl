deb1b3084394153e3932d19ff8d3978ab292fb9f
Gary B. Genett <me@garybgenett.net>
2013-03-08 16:50:10 -0800

    Added "gtasks_export.pl" script.

---
+++ scripts/gtasks_export.pl
@@ -0,0 +1,401 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+################################################################################
+# https://developers.google.com/accounts/docs/OAuth2InstalledApp
+# https://developers.google.com/google-apps/tasks/v1/reference
+################################################################################
+# http://search.cpan.org/~jesse/WWW-Mechanize-1.72/lib/WWW/Mechanize.pm
+# http://search.cpan.org/~gaas/libwww-perl-6.04/lib/LWP/UserAgent.pm
+# http://search.cpan.org/~gaas/HTTP-Message-6.06/lib/HTTP/Request.pm
+# http://search.cpan.org/~makamaka/JSON-2.53/lib/JSON.pm
+################################################################################
+
+use Data::Dumper;
+sub DUMPER {
+	my $DUMP = shift;
+	local $Data::Dumper::Purity = 1;
+	print "<-- DUMPER " . ("-" x 30) . ">\n";
+	print Dumper(${DUMP});
+	print "<-- DUMPER " . ("-" x 30) . ">\n";
+	return(0);
+};
+
+########################################
+
+use WWW::Mechanize;
+my $mech = WWW::Mechanize->new(
+	"agent" => "Mozilla/5.0",
+);
+
+use HTTP::Request;
+use JSON::PP;
+
+################################################################################
+
+my $FILE		= "tasks";
+
+my $SCOPE		= "https://www.googleapis.com/auth/tasks";
+my $URL			= "https://www.googleapis.com/tasks/v1";
+
+my $EXPORT_JSON		= "1";
+my $EXPORT_CSV		= "1";
+my $EXPORT_TXT		= "1";
+
+my $CSV_FIELDS = [
+	"title",
+
+	"due",
+	"status",
+	"completed",
+	"deleted",
+	"hidden",
+	"notes",
+
+	"kind",
+	"id",
+	"etag",
+	"selfLink",
+	"updated",
+
+	"parent",
+	"position",
+	"links",
+];
+
+my $HIDE_COMPLETED	= "0";
+my $HIDE_DELETED	= "0";
+
+########################################
+
+our $USERNAME;
+our $PASSWORD;
+our $CLIENTID;
+our $CLSECRET;
+our $REDIRECT;
+do(".auth");
+
+our $CODE;
+our $REFRESH;
+our $ACCESS;
+do(".token");
+
+################################################################################
+
+sub refresh_tokens {
+	if (!${CODE} || !${REFRESH}) {
+		$mech->get("https://accounts.google.com/ServiceLogin");
+		$mech->form_id("gaia_loginform");
+		$mech->field("Email",	${USERNAME});
+		$mech->field("Passwd",	${PASSWORD});
+		$mech->submit();
+
+		$mech->get("https://accounts.google.com/o/oauth2/auth"
+			. "?client_id=${CLIENTID}"
+			. "&redirect_uri=${REDIRECT}"
+			. "&scope=${SCOPE}"
+			. "&response_type=code"
+		);
+		$mech->submit_form(
+			"form_id"	=> "submit_access_form",
+			"fields"	=> {"submit_access" => "true"},
+		);
+		$CODE = $mech->content();
+		$CODE =~ s|^.*<input id="code" type="text" readonly="readonly" value="||s;
+		$CODE =~ s|".*$||s;
+
+		$mech->post("https://accounts.google.com/o/oauth2/token", {
+			"code"			=> ${CODE},
+			"client_id"		=> ${CLIENTID},
+			"client_secret"		=> ${CLSECRET},
+			"redirect_uri"		=> ${REDIRECT},
+			"grant_type"		=> "authorization_code",
+		});
+		$REFRESH = decode_json($mech->content());
+		$REFRESH = $REFRESH->{"refresh_token"};
+	};
+
+	$mech->post("https://accounts.google.com/o/oauth2/token", {
+		"refresh_token"		=> ${REFRESH},
+		"client_id"		=> ${CLIENTID},
+		"client_secret"		=> ${CLSECRET},
+		"grant_type"		=> "refresh_token",
+	});
+	$ACCESS = decode_json($mech->content());
+	$ACCESS = $ACCESS->{"access_token"};
+
+	open(OUTPUT, ">", ".token");
+	print OUTPUT "our \$CODE    = '${CODE}';\n";
+	print OUTPUT "our \$REFRESH = '${REFRESH}';\n";
+	close(OUTPUT);
+
+	print "CODE:    ${CODE}\n";
+	print "REFRESH: ${REFRESH}\n";
+	print "ACCESS:  ${ACCESS}\n";
+
+	$mech->add_header("Authorization" => "Bearer ${ACCESS}");
+
+	return(0);
+};
+
+########################################
+
+sub manage_cruft {
+	my $output;
+
+	print "\nManaging cruft...\n";
+
+	$mech->get("${URL}/lists/\@default/tasks"
+		. "?maxResults=1000000"
+		. "&showCompleted=true"
+		. "&showDeleted=true"
+		. "&showHidden=true"
+	);
+	$output = decode_json($mech->content());
+
+	foreach my $task (@{$output->{"items"}}) {
+#>>> BUG IN GOOGLE TASKS API!
+#>>> http://code.google.com/a/google.com/p/apps-api-issues/issues/detail?id=2888
+#>>> SHOULD JUST BE ABLE TO MOVE THEM TO A "PURGE" LIST FOR MANUAL DELETION
+		if ($task->{"title"} =~ "\n") {
+			$task->{"title"} =~ s/\n//g;
+			print "rescuing $task->{'id'} $task->{'title'}\n";
+		};
+
+		if (	!$task->{"completed"}	&&
+			!$task->{"title"}	&&
+			!$task->{"notes"}	&&
+			!$task->{"due"}
+		) {
+			print "clearing $task->{'id'} $task->{'title'}\n";
+			$mech->request(HTTP::Request->new(
+				"PATCH", $task->{"selfLink"}, ["Content-Type", "application/json"], encode_json({
+					"title"		=> "0",
+					"status"	=> "completed",
+					"deleted"	=> "0",
+				})
+			));
+		};
+
+		if ((	$task->{"completed"}	||
+			$task->{"deleted"}	) && (
+			$task->{"title"}	||
+			$task->{"notes"}	||
+			$task->{"due"}		)
+		) {
+			print "reviving $task->{'id'} $task->{'title'}\n";
+			$mech->request(HTTP::Request->new(
+				"PATCH", $task->{"selfLink"}, ["Content-Type", "application/json"], encode_json({
+					"title"		=> "[" . sprintf("%03d", int(rand(10**3))) . "]:[$task->{'title'}]",
+					"status"	=> "needsAction",
+					"completed"	=> undef,
+					"deleted"	=> "0",
+				})
+			));
+		};
+#>>>
+	};
+
+	$mech->post("${URL}/lists/\@default/clear");
+
+	return(0);
+};
+
+########################################
+
+sub export_files {
+	my $output;
+
+	(${EXPORT_JSON}) && (open(JSON, ">", "${FILE}.json") || die());
+	(${EXPORT_CSV})  && (open(CSV,  ">", "${FILE}.csv")  || die());
+	(${EXPORT_TXT})  && (open(TXT,  ">", "${FILE}.txt")  || die());
+
+	if (${EXPORT_CSV}) {
+		print CSV "\"indent\",";
+		foreach my $field (@{$CSV_FIELDS}) {
+			print CSV "\"${field}\",";
+		};
+		print CSV "\n";
+	};
+
+	$mech->get("${URL}/users/\@me/lists"
+		. "?maxResults=1000000"
+	);
+	if (${EXPORT_JSON}) {
+		print JSON "\n" . ("#" x 10) . "[ LISTS ]" . ("#" x 10) . "\n\n";
+		print JSON $mech->content();
+	};
+	$output = decode_json($mech->content());
+
+#>>> BUG IN PERL!
+#>>> http://www.perlmonks.org/?node_id=490213
+	my @array = @{$output->{"items"}};
+	foreach my $task (sort({$a->{"title"} cmp $b->{"title"}} @{array})) {
+#>>>
+		&export_files_item(${task}, "-", "-");
+
+		$mech->get("${URL}/lists/$task->{'id'}/tasks"
+			. "?maxResults=1000000"
+			. "&showCompleted=true"
+			. "&showDeleted=true"
+			. "&showHidden=true"
+		);
+		if (${EXPORT_JSON}) {
+			print JSON "\n" . ("#" x 10) . "[ $task->{'title'} ]" . ("#" x 10) . "\n\n";
+			print JSON $mech->content();
+		};
+		$output = decode_json($mech->content());
+
+		&export_files_list(${output});
+	};
+
+	(${EXPORT_JSON}) && print JSON "\n" . ("#" x 10) . "[ END OF FILE ]" . ("#" x 10) . "\n";
+	(${EXPORT_TXT})  && print TXT  "\n" . ("#" x 10) . "[ END OF FILE ]" . ("#" x 10) . "\n";
+
+	(${EXPORT_JSON}) && (close(JSON) || die());
+	(${EXPORT_CSV})  && (close(CSV)  || die());
+	(${EXPORT_TXT})  && (close(TXT)  || die());
+
+	return(0);
+};
+
+########################################
+
+sub export_files_list {
+	my $list	= shift;
+	my $tree	= {};
+
+	foreach my $task (@{$list->{"items"}}) {
+		(${HIDE_COMPLETED}	&& $task->{"completed"}		) && (next());
+		(${HIDE_DELETED}	&& $task->{"deleted"}		) && (next());
+		if (!exists($task->{"parent"})) {
+			$tree->{$task->{"id"}} = {
+				"node" => ${task},
+				"pos" => $task->{"position"},
+			};
+		} else {
+			$tree->{$task->{"parent"}}{"sub"}{$task->{"id"}} = {
+				"node" => ${task},
+				"pos" => $task->{"position"},
+			};
+		};
+	};
+
+	&export_files_list_tree(${tree}, ${tree}, "0");
+
+	return(0);
+};
+
+########################################
+
+sub export_files_list_tree {
+	my $root_tree	= shift;
+	my $tree	= shift;
+	my $indent	= shift;
+	my $key;
+
+	foreach $key (keys($tree)) {
+		if (!exists($tree->{$key}->{"pos"})) {
+			$tree->{$key}->{"pos"} = "";
+		};
+	};
+
+	foreach $key (sort({$tree->{$a}{"pos"} cmp $tree->{$b}{"pos"}} keys($tree))) {
+		if ($tree->{$key}->{"pos"}) {
+			&export_files_item($tree->{$key}{"node"}, ${indent}, "");
+			if (exists($root_tree->{$key}{"sub"})) {
+				&export_files_list_tree(${root_tree}, $root_tree->{$key}->{"sub"}, (${indent} + 1));
+			};
+		};
+	};
+
+	return(0);
+};
+
+########################################
+
+sub export_files_item {
+	my $task	= shift;
+	my $indent	= shift;
+	my $empty	= shift;
+
+	if (${EXPORT_CSV}) {
+		print CSV "\"${indent}\",";
+		foreach my $field (@{$CSV_FIELDS}) {
+			if(exists($task->{$field})) {
+				my $output = $task->{$field};
+				$output =~ s/^"//g;
+				$output =~ s/"$//g;
+				print CSV "\"${output}\",";
+			} else {
+				print CSV "\"${empty}\",";
+			};
+		};
+		print CSV "\n";
+	};
+
+	if (${EXPORT_TXT}) {
+		if (${indent} !~ /\d+/) {
+			print TXT "\n" . ("#" x 10) . "[ $task->{'title'} ]" . ("#" x 10) . "\n\n";
+		} else {
+			my $tabs = ("\t" x (${indent} + 2));
+			print TXT ("\t" x ${indent});
+
+			if ($task->{"completed"}) {
+				print TXT "x";
+			} elsif ($task->{"deleted"}) {
+				print TXT ">";
+			} else {
+				print TXT "*";
+			};
+
+			foreach my $field (qw/
+				completed
+				due
+				title
+				notes
+			/) {
+				if(exists($task->{$field})) {
+					my $output = $task->{$field};
+					if (${field} eq "due") {
+						$output =~ s/T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9A-Z]{4}$//g;
+					};
+					if (${field} eq "notes") {
+						$output =~ s|^([ ]+)|("\t" x (length(${1}) / 2))|egm;
+						$output =~ s/^/${tabs}/gm;
+						$output =~ s/^/\n/;
+					};
+					if (${field} ne "notes") {
+						print TXT " ";
+					};
+					print TXT "${output}";
+				};
+			};
+
+			print TXT "\n";
+		};
+	};
+
+	return(0);
+};
+
+################################################################################
+
+&refresh_tokens();
+&manage_cruft();
+&export_files();
+
+########################################
+
+if (${EXPORT_TXT}) {
+	open(TXT, "<", "${FILE}.txt") || die();
+	print <TXT>;
+	close(TXT) || die();
+};
+
+########################################
+
+exit(0);
+################################################################################
+# end of file
+################################################################################
